5251891811730284b46d9f66c8618b0ab27ca6bc
osbornjr <osbornjr@rose-hulman.edu>
2017-10-25 12:37:42 -0400

    josh m1 done

--- sandbox/osbornjr/motors/m1_drive_timed.py
+++ sandbox/osbornjr/motors/m1_drive_timed.py
@@ -87,6 +87,8 @@ def main():
     while drive_time != 0:
         speed_degrees = int(input("Enter a speed (0 to 900 dps): "))
         distance = int(input("Distance to travel(inches): "))
+        if speed_degrees == 0 or distance == 0:
+            break
         left_motor.run_forever(speed_sp=speed_degrees)
         right_motor.run_forever(speed_sp=speed_degrees)
         speed_inches = (4/360)*speed_degrees
@@ -104,21 +106,21 @@ def main():
 # ----------------------------------------------------------------------
 main()
 
-# TODO: 4. Change the input questions from:
+# Done: 4. Change the input questions from:
 #   Enter a speed for the left motor (0 to 900 dps):
 #   Enter a speed for the right motor (0 to 900 dps):
 #   Enter a time to drive (seconds):
 # to:
 #   Enter a speed (0 to 900 dps):
 #   Distance to travel (inches):
-# TODO: 5. Write the code necessary to make the robot drive at that speed going roughly that distance.
+# Done: 5. Write the code necessary to make the robot drive at that speed going roughly that distance.
 #   Note, in this module, you are REQUIRED to use the pattern...
 #      run_forever()
 #      time.sleep(some_amount)
 #      stop()
 #   You may NOT use the advanced motor commands at this time like: run_to_abs_pos, run_to_rel_pos, or run_timed.
-# TODO: 6. Modify the program so that it will exit immediately if the answer to   any   question is 0.
-# TODO: 7. Formally test your work. When you think you have the problem complete run these tests to be sure:
+# Done: 6. Modify the program so that it will exit immediately if the answer to   any   question is 0.
+# Done: 7. Formally test your work. When you think you have the problem complete run these tests to be sure:
 #   200 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   400 dps 24 inches (make sure it drives within 6 inches of the target distance)
 #   800 dps 24 inches (make sure it drives within 6 inches of the target distance)
