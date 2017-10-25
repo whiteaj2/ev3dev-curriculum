653d070a81824b72d6114b01bc34405a028a1198
whiteaj2 <whiteaj2@rose-hulman.edu>
2017-10-25 12:54:05 -0400

    finsihed m2

--- sandbox/whiteaj2/motors/m2_drive_encoders.py
+++ sandbox/whiteaj2/motors/m2_drive_encoders.py
@@ -4,16 +4,16 @@ This module lets you practice using the encoder to determine distances while blo
 
 You will now use a run_to_rel_pos command to implement the action drive inches action.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Andrew White.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Copy the contents of your m1_drive_timed.py and paste that text into this file below these comments.
+# DONE: 2. Copy the contents of your m1_drive_timed.py and paste that text into this file below these comments.
 #   If your program says and prints anything at the start change it to print and say "Drive using encoders"
 
-# TODO: 3. Add a beep after the drive motors stop (see code below).  Test your code to hear the beep AFTER movement.
+# DONE: 3. Add a beep after the drive motors stop (see code below).  Test your code to hear the beep AFTER movement.
 #   ev3.Sound.beep().wait()
 
-# TODO: 4. Instead of using the run_forever, time.sleep, stop pattern switch to using the run_to_rel_pos command.
+# DONE: 4. Instead of using the run_forever, time.sleep, stop pattern switch to using the run_to_rel_pos command.
 #   You will need to determine the position_sp value to pass into the run_to_rel_pos command as a named argument.
 #   Assume the diameter of the wheel is 1.3" (close enough).  A 1.3" diameter wheel results in approximately a 4"
 #     circumference, so 360 degrees = 4 inches of travel.
@@ -29,9 +29,9 @@ Authors: David Fisher and PUT_YOUR_NAME_HERE.
 #        -- speed_sp
 #        -- stop_action
 
-# TODO: 5. Make sure the beep happens AFTER the motors stop.  Use the wait_while command to block code execution.
+# DONE: 5. Make sure the beep happens AFTER the motors stop.  Use the wait_while command to block code execution.
 
-# TODO: 6. Formally test your work. When you think you have the problem complete run these tests:
+# DONE: 6. Formally test your work. When you think you have the problem complete run these tests:
 #   200 dps 24 inches (make sure it drives within 2 inches of the target distance)
 #   400 dps 24 inches (make sure it drives within 2 inches of the target distance)
 #   800 dps 24 inches (make sure it drives within 2 inches of the target distance)
@@ -43,5 +43,58 @@ Authors: David Fisher and PUT_YOUR_NAME_HERE.
 # TODO: 7. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
 #
 # Observations you should make, run_to_rel_pos is easier to use since it uses encoders that are independent of speed.
+
+import ev3dev.ev3 as ev3
+import time
+
+
+def main():
+    print("--------------------------------------------")
+    print("Drive Encoders")
+    print("--------------------------------------------")
+    ev3.Sound.speak("Drive Encoders").wait()
+
+    # Connect two large motors on output ports B and C
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
+    while True:
+        print("----------------------")
+        sp = int(input("Enter a speed (0 to 900 dps): "))
+        if sp == 0:
+            break
+        distance = int(input("Enter a distance to travel(inches): "))
+        if distance == 0:
+            break
+        print("----------------------")
+        #Unused code from m1
+        #speedConversion = sp * (4/360) #converts speed to inches per second
+        #driveTime = distance / speedConversion
+        #left_motor.run_forever(speed_sp=sp)
+        #right_motor.run_forever(speed_sp=sp)
+        #time.sleep(driveTime)
+        #left_motor.stop(stop_action="brake")
+        #right_motor.stop(stop_action="brake")
+
+
+        degreesToDrive = distance * 90
+        left_motor.run_to_rel_pos(position_sp=degreesToDrive,speed_sp=sp)
+        right_motor.run_to_rel_pos(position_sp=degreesToDrive,speed_sp=sp)
+        left_motor.wait_while(left_motor.STATE_RUNNING)
+        right_motor.wait_while(right_motor.STATE_RUNNING)
+        ev3.Sound.beep().wait()
+
+    print("Goodbye!")
+    ev3.Sound.speak("Goodbye").wait()
+
+
+# ----------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# ----------------------------------------------------------------------
+main()
 
 
