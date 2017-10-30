91b167cad6180042878b2ce34ac08f135b1a0da8
whiteaj2 <whiteaj2@rose-hulman.edu>
2017-10-30 12:10:33 -0400

    Finished drive polygon

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -42,7 +42,7 @@ class Snatch3r(object):
 
 
     def turn_degrees(self, degrees_to_turn, turn_speed_sp):
-        print("Turning " + str(degrees_to_turn) + " degrees")
+       ev3.Sound.speak("Turning")
         self.left_motor.run_to_rel_pos(position_sp=-4*1.24*degrees_to_turn, speed_sp=turn_speed_sp)
         self.right_motor.run_to_rel_pos(position_sp=4*1.24*degrees_to_turn, speed_sp=turn_speed_sp)
         self.left_motor.wait_while(self.left_motor.STATE_RUNNING)
