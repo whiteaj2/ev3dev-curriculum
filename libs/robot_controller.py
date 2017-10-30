5459493a9b464c28ce25b608a56db730c7555d32
whiteaj2 <whiteaj2@rose-hulman.edu>
2017-10-30 08:06:26 -0400

    Finished drive polygon

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -32,6 +32,7 @@ class Snatch3r(object):
 
     def drive_inches(self, distance, sp):
         ev3.Sound.speak("Driving")
+        print("Driving " + str(distance) + " inches at " + str(sp))
         self.left_motor.run_to_rel_pos(position_sp=distance*90, speed_sp=sp)
         self.right_motor.run_to_rel_pos(position_sp=distance * 90, speed_sp=sp)
         self.left_motor.wait_while(self.left_motor.STATE_RUNNING)
@@ -41,6 +42,7 @@ class Snatch3r(object):
 
 
     def turn_degrees(self, degrees_to_turn, turn_speed_sp):
+        print("Turning " + str(degrees_to_turn) + " degrees")
         self.left_motor.run_to_rel_pos(position_sp=-4*1.24*degrees_to_turn, speed_sp=turn_speed_sp)
         self.right_motor.run_to_rel_pos(position_sp=4*1.24*degrees_to_turn, speed_sp=turn_speed_sp)
         self.left_motor.wait_while(self.left_motor.STATE_RUNNING)
--- sandbox/whiteaj2/motors/m5_drive_polygon.py
+++ sandbox/whiteaj2/motors/m5_drive_polygon.py
@@ -5,8 +5,8 @@ This module lets you integrate your work on drive_inches and turn_degrees into a
 You will ask the user for how many sides they would like in their polygon, the length of each side, and a speed.
 Then your robot will drive that polygon shape.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Andrew  White.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import robot_controller as robo
@@ -20,21 +20,28 @@ def main():
     robot = robo.Snatch3r()
 
     while True:
+        print('---------------------------------------------------')
         speed_deg_per_second = int(input("Speed (0 to 900 dps): "))
         if speed_deg_per_second == 0:
             break
 
+        print('---------------------------------------------------')
         sides = int(input("Number of sides: "))
         # Tip for later, try a negative value for Number of sides: to drive CW around the polygon instead of CCW.
         if sides == 0:
             break
         turn_amount = 360 / sides
 
+        print('---------------------------------------------------')
         edge_length_in = int(input("Length of each edge (inches): "))
         if edge_length_in == 0:
             break
+        for k in range(sides):
+            robot.drive_inches(edge_length_in)
+            robot.turn_degrees(turn_amount)
 
-        # TODO: 2. Individually implement the code here to use your drive_inches and turn_degrees library methods to
+
+        # DONE: 2. Individually implement the code here to use your drive_inches and turn_degrees library methods to
         # drive a polygon with the correct number of sides. (Hint: You will add 3 lines of code. What are they?).
 
         # TODO: 3. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.
