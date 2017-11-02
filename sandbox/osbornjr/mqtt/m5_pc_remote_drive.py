25fd2d8259312a8672bfed6063c17d936898cc51
osbornjr <osbornjr@rose-hulman.edu>
2017-11-02 12:52:33 -0400

    Mqtt done

--- sandbox/osbornjr/mqtt/m5_pc_remote_drive.py
+++ sandbox/osbornjr/mqtt/m5_pc_remote_drive.py
@@ -22,8 +22,8 @@ You will need to have the following features:
 
 You can start by running the code to see the GUI, but don't expect button clicks to do anything useful yet.
 
-Authors: David Fisher and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and Joshua Osborne.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import tkinter
 from tkinter import ttk
@@ -32,9 +32,10 @@ import mqtt_remote_method_calls as com
 
 
 def main():
-    # TODO: 2. Setup an mqtt_client.  Notice that since you don't need to receive any messages you do NOT need to have
+    # Done: 2. Setup an mqtt_client.  Notice that since you don't need to receive any messages you do NOT need to have
     # a MyDelegate class.  Simply construct the MqttClient with no parameter in the constructor (easy).
-    mqtt_client = None  # Delete this line, it was added temporarily so that the code we gave you had no errors.
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3(lego_robot_number=8)
 
     root = tkinter.Tk()
     root.title("MQTT Remote")
@@ -54,7 +55,7 @@ def main():
     right_speed_entry.insert(0, "600")
     right_speed_entry.grid(row=1, column=2)
 
-    # TODO: 3. Implement the callbacks for the drive buttons. Set both the click and shortcut key callbacks.
+    # Done: 3. Implement the callbacks for the drive buttons. Set both the click and shortcut key callbacks.
     #
     # To help get you started the arm up and down buttons have been implemented.
     # You need to implement the five drive buttons.  One has been writen below to help get you started but is commented
@@ -63,43 +64,53 @@ def main():
     forward_button = ttk.Button(main_frame, text="Forward")
     forward_button.grid(row=2, column=1)
     # forward_button and '<Up>' key is done for your here...
-    # forward_button['command'] = lambda: some_callback1(mqtt_client, left_speed_entry, right_speed_entry)
-    # root.bind('<Up>', lambda event: some_callback1(mqtt_client, left_speed_entry, right_speed_entry))
+    forward_button['command'] = lambda: send_forward(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<w>', lambda event: send_forward(mqtt_client, left_speed_entry, right_speed_entry))
 
     left_button = ttk.Button(main_frame, text="Left")
     left_button.grid(row=3, column=0)
+    left_button['command'] = lambda: send_left(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<a>', lambda event: send_left(mqtt_client, left_speed_entry, right_speed_entry))
     # left_button and '<Left>' key
 
     stop_button = ttk.Button(main_frame, text="Stop")
     stop_button.grid(row=3, column=1)
+    stop_button['command'] = lambda: send_stop(mqtt_client)
+    root.bind('<space>', lambda event: send_stop(mqtt_client))
     # stop_button and '<space>' key (note, does not need left_speed_entry, right_speed_entry)
 
     right_button = ttk.Button(main_frame, text="Right")
     right_button.grid(row=3, column=2)
+    right_button['command'] = lambda: send_right(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<d>', lambda event: send_right(mqtt_client, left_speed_entry, right_speed_entry))
     # right_button and '<Right>' key
 
     back_button = ttk.Button(main_frame, text="Back")
     back_button.grid(row=4, column=1)
+    back_button['command'] = lambda: send_back(mqtt_client, left_speed_entry, right_speed_entry)
+    root.bind('<s>', lambda event: send_back(mqtt_client, left_speed_entry, right_speed_entry))
     # back_button and '<Down>' key
 
     up_button = ttk.Button(main_frame, text="Up")
     up_button.grid(row=5, column=0)
     up_button['command'] = lambda: send_up(mqtt_client)
-    root.bind('<u>', lambda event: send_up(mqtt_client))
+    root.bind('<e>', lambda event: send_up(mqtt_client))
 
     down_button = ttk.Button(main_frame, text="Down")
     down_button.grid(row=6, column=0)
     down_button['command'] = lambda: send_down(mqtt_client)
-    root.bind('<j>', lambda event: send_down(mqtt_client))
+    root.bind('<q>', lambda event: send_down(mqtt_client))
 
     # Buttons for quit and exit
     q_button = ttk.Button(main_frame, text="Quit")
     q_button.grid(row=5, column=2)
     q_button['command'] = (lambda: quit_program(mqtt_client, False))
+    root.bind('<Escape>', lambda  event: quit_program(mqtt_client, False))
 
     e_button = ttk.Button(main_frame, text="Exit")
     e_button.grid(row=6, column=2)
     e_button['command'] = (lambda: quit_program(mqtt_client, True))
+    root.bind('<m>', lambda event: quit_program(mqtt_client, True))
 
     root.mainloop()
 
@@ -107,7 +118,28 @@ def main():
 # ----------------------------------------------------------------------
 # Tkinter callbacks
 # ----------------------------------------------------------------------
-# TODO: 4. Implement the functions for the drive button callbacks.
+# DOne: 4. Implement the functions for the drive button callbacks.
+
+def send_forward(mqtt_client, left_speed, right_speed):
+    print("forward")
+    mqtt_client.send_message("drive",[int(left_speed.get()), int(right_speed.get())])
+
+def send_back(mqtt_client, left_speed, right_speed):
+    print("back")
+    mqtt_client.send_message("drive", [-int(left_speed.get()), -int(right_speed.get())])
+
+def send_left(mqtt_client, left_speed, right_speed):
+    print("left")
+    mqtt_client.send_message("drive", [-int(left_speed.get()), int(right_speed.get())])
+
+def send_right(mqtt_client, left_speed, right_speed):
+    print("right")
+    mqtt_client.send_message("drive", [int(left_speed.get()), -int(right_speed.get())])
+
+def send_stop(mqtt_client):
+    print("stop")
+    mqtt_client.send_message("stop")
+
 
 # TODO: 5. Call over a TA or instructor to sign your team's checkoff sheet and do a code review.  This is the final one!
 #
@@ -124,7 +156,6 @@ def send_up(mqtt_client):
 def send_down(mqtt_client):
     print("arm_down")
     mqtt_client.send_message("arm_down")
-
 
 # Quit and Exit button callbacks
 def quit_program(mqtt_client, shutdown_ev3):
