0b2971ccfac65d2febd929ca618a755cf179cab4
osbornjr <osbornjr@rose-hulman.edu>
2017-11-08 23:17:41 -0500

    We are #1

--- projects/osbornjr/ev3_project_ev3.py
+++ projects/osbornjr/ev3_project_ev3.py
@@ -3,6 +3,7 @@ import time
 import robot_controller as robo
 import mqtt_remote_method_calls as com
 import math
+from PIL import Image
 
 robot = robo.Snatch3r()
 
@@ -14,10 +15,13 @@ class MyDelegateEV3(object):
         self.running = True
         self.mqtt_client = None
         self.lcd = ev3.Screen()
-        self.scale = 0.10
+        self.scale = 0.05
 
     def drive_bot(self, speed):
         self.speed = speed
+
+    def turn_bot(self, turn_speed):
+        self.turn_speed = turn_speed
 
     def on_circle_draw(self, x, y):
 
@@ -26,23 +30,29 @@ class MyDelegateEV3(object):
         self.points += [(x,y)]
         print(self.points)
 
-    def loop_forever(self):
-        btn = ev3.Button()
-        self.running = True
-        while not btn.backspace and self.running:
-            # Do nothing while waiting for commands
-            time.sleep(0.01)
-        self.mqtt_client.close()
-        # Copied from robot.shutdown
-        print("Goodbye")
-        ev3.Sound.speak("Goodbye").wait()
-
     def connect_dots(self):
-        self.angle = math.atan((self.points[1][1] - self.points[0][1]) / (self.points[1][0] - self.points[0][0])) * (180 / math.pi)
-        robot.turn_degrees(self.angle, 100)
-        time.sleep(1)
-        self.distance = math.sqrt((self.points[1][1] - self.points[0][1]) ** 2 + (self.points[1][0] - self.points[0][0]) ** 2) * self.scale
-        robot.drive_inches(self.distance, self.speed)
+        for k in range(len(self.points)-1):
+            self.angle = math.atan((self.points[k][1] - self.points[k-1][1]) / (self.points[k][0] - self.points[k-1][0])) * (180 / math.pi)
+            robot.turn_degrees(self.angle, self.turn_speed)
+            if self.angle < 180:
+                print("Left turn neccesary")
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
+            elif self.angle > 180:
+                print("Right turn neccesary")
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.GREEN)
+            else:
+                print("No turn neccesary")
+                ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.AMBER)
+                ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.AMBER)
+            time.sleep(1)
+            self.distance = math.sqrt((self.points[k][1] - self.points[k-1][1]) ** 2 + (self.points[k][0] - self.points[k-1][0]) ** 2) * self.scale
+            robot.drive_inches(self.distance, self.speed)
+            time.sleep(1)
+            print("Turn Angle: ", self.angle, "Drive Distance", self.distance)
+        print("Final Destination Reached")
+        ev3.Sound.speak("Destination Reached").wait()
 
     def clear(self):
         self.points.clear()
@@ -53,10 +63,19 @@ def main():
     print("-----------------------------------")
     ev3.Sound.speak("ev3_project_ev3").wait()
 
-    my_delegate = MyDelegateEV3()
-    mqtt_client = com.MqttClient(my_delegate)
-    mqtt_client.connect_to_pc(lego_robot_number=8)
+    while not robot.touch_sensor.is_pressed:
 
-    my_delegate.loop_forever()
+        my_delegate = MyDelegateEV3()
+        mqtt_client = com.MqttClient(my_delegate)
+        mqtt_client.connect_to_pc(lego_robot_number=8)
+
+        teary_eyes = Image.open("/home/robot/csse120/assets/images/ev3_lego/eyes_tear.bmp")
+        my_delegate.lcd.image.paste(teary_eyes, (0, 0))
+        my_delegate.lcd.update()
+
+        time.sleep(0.1)
+
+    print("Goodbye")
+    ev3.Sound.speak("Goodbye").wait()
 
 main()
--- projects/osbornjr/ev3_project_pc.py
+++ projects/osbornjr/ev3_project_pc.py
@@ -28,43 +28,83 @@ def main():
     print("-----------------------------------")
 
     root = tkinter.Tk()
-    root.title = "Draw Picture"
+    root.title("Draw Picture")
+
+    # Code for the main frame of the Tkinter window
 
     main_frame = ttk.Frame(root, padding=2)
     main_frame.grid()
 
-    instructions = "Click the window to make a point."
+    # Instructions for each key
+
+    instructions = "Create a point by clicking"
     label = ttk.Label(main_frame, text=instructions)
-    label.grid(columnspan=2, row=0, column=1)
+    label.grid(columnspan=1, row=0, column=1)
 
-    instructions2 = "Click connect to connect the points"
+    instructions2 = "Connect the Points"
     label2 = ttk.Label(main_frame, text=instructions2)
-    label2.grid(columnspan=2, row=1, column=1)
+    label2.grid(columnspan=1, row=1, column=1)
 
-    instructions3 = "Slide the slider for speed"
+    instructions3 = "Set Robot Speed"
     label3 = ttk.Label(main_frame, text=instructions3)
-    label3.grid(columnspan=2, row=1, column=4)
+    label3.grid(columnspan=1, row=1, column=3)
+
+    instructions4 = "Set Robot Turn Speed"
+    label4 = ttk.Label(main_frame, text=instructions4)
+    label4.grid(columnspan=1, row=1, column=4)
+
+    instructions5 = "Exit Program"
+    label5 = ttk.Label(main_frame, text=instructions5)
+    label5.grid(columnspan=1, row=1, column=2)
+
+    instructions6 = "Clear Points"
+    label6 = ttk.Label(main_frame, text=instructions6)
+    label6.grid(columnspan=1, row=1, column=0)
+
+    # Creating the slider widgets
 
     speed_slider = Scale(main_frame, from_=0, to=900, orient=HORIZONTAL)
-    speed_slider.grid(row=3, column=4)
-    speed_slider["command"] = lambda event: send_drive_command(mqtt_client, speed_slider.get())
+    speed_slider.grid(row=3, column=3)
+    speed_slider["command"] = lambda event: send_drive_speed(mqtt_client, speed_slider.get())
+
+    turn_slider = Scale(main_frame, from_=0, to=900, orient=HORIZONTAL)
+    turn_slider.grid(row=3, column=4)
+    turn_slider["command"] = lambda event: send_turn_speed(mqtt_client, turn_slider.get())
+
+    # Creating the canvas
 
     canvas = tkinter.Canvas(main_frame, background="lightgray", width=800, height=500)
     canvas.grid(columnspan=5)
 
-    canvas.bind("<Button-1>", lambda event: left_mouse_click(event, mqtt_client))
+    canvas.bind("<Button-1>", lambda event: left_mouse_click(event, mqtt_client, list_box))
+
+    # Creating the buttons
 
     connect_button = ttk.Button(main_frame, text="Connect")
     connect_button.grid(row=3, column=1)
     connect_button["command"] = lambda: connect_dots(canvas, mqtt_client)
+    root.bind('<space>', lambda event: connect_dots(canvas, mqtt_client))
 
     clear_button = ttk.Button(main_frame, text="Clear")
     clear_button.grid(row=3, column=0)
-    clear_button["command"] = lambda: clear(canvas, mqtt_client)
+    clear_button["command"] = lambda: clear(canvas, mqtt_client, list_box)
+    root.bind('<e>', lambda event: clear(canvas, mqtt_client, list_box))
 
     quit_button = ttk.Button(main_frame, text="Quit")
     quit_button.grid(row=3, column=2)
     quit_button["command"] = lambda: quit_program(mqtt_client)
+    root.bind('<Escape>', lambda event: quit_program(mqtt_client))
+
+    #Code for the Extra frame
+
+    ext_frame = ttk.Frame(main_frame, padding=2)
+    ext_frame.grid(row=4, column=6)
+
+    list_box = Listbox(ext_frame)
+    list_box.insert(END, "List of points (X, Y)")
+    list_box.grid(row=0, column=0, columnspan=1)
+
+    #Sending commands to the delegate
 
     my_delegate = MyDelegatePC(canvas)
     mqtt_client = com.MqttClient(my_delegate)
@@ -77,12 +117,16 @@ def quit_program(mqtt_client):
     mqtt_client.close()
     exit()
 
-def left_mouse_click(event, mqtt_client):
+# Function to draw circles client side, and send coordinates to the delegate
+# Also populates a list of all the points to be seen client-side
+def left_mouse_click(event, mqtt_client, list_box):
     global points
     """ Draws a circle onto the canvas (one way or another). """
     print("You clicked location ({},{})".format(event.x, event.y))
 
     points += [(event.x, event.y)]
+
+    list_box.insert(END, points[len(points)-1])
 
     # Examples... "red", "green", "blue", "yellow", "aquamarine", "magenta", "navy", "orange"
     my_color = "navy"  # Make your color unique
@@ -94,27 +138,37 @@ def left_mouse_click(event, mqtt_client):
 
     mqtt_client.send_message("on_circle_draw",[event.x, event.y])
 
-
-def clear(canvas, mqtt_client):
+# Clears the list of points client and over the delegate
+def clear(canvas, mqtt_client, list_box):
     """Clears the canvas contents"""
     canvas.delete("all")
     mqtt_client.send_message("clear")
+    for k in range(len(points)):
+        list_box.delete(k+1, END)
     points.clear()
 
+# Tells the robot to "connect the dots" or draw the picture that has been sent using the points
 def connect_dots(canvas, mqtt_client):
     if len(points) > 1:
         for k in range(len(points)-1):
             mqtt_client.send_message("connect_dots")
             canvas.create_line(points[k][0], points[k][1], points[k+1][0], points[k+1][1], fill="black", dash=(4,2))
-        print(points)
+        print("Sent list of points: ", points)
 
     else:
         print("Error: Invalid number of points. Draw 2 or more points to connect.")
 
-def send_drive_command(mqtt_client, drive_speed):
+# Sends the drive speed to the delegate using input from the slider
+def send_drive_speed(mqtt_client, drive_speed):
     print("Sending drive speed = {}".format(drive_speed))
     mqtt_client.send_message("drive_bot",[drive_speed])
+    time.sleep(0.10)
 
+# Sends the turn speed to the delegate using input from the slider
+def send_turn_speed(mqtt_client, turn_speed):
+    print("Sending turn speed = {}".format(turn_speed))
+    mqtt_client.send_message("turn_bot", [turn_speed])
+    time.sleep(0.10)
 
 main()
 
